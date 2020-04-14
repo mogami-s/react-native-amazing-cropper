11ae71e5a1dbe5d33815fb2faa1fd25ea99d7da4
s.mogami <54295714+mogami-s@users.noreply.github.com>
2020-04-14 07:52:43 +0530

    Update Cropper.component.style.ts
    changed grid style and border color

--- src/components/Cropper/Cropper.component.style.ts
+++ src/components/Cropper/Cropper.component.style.ts
@@ -28,7 +28,7 @@ export default StyleSheet.create({
   },
   gridColumn: {
     flex: 1,
-    borderWidth: 1,
+//     borderWidth: 1,
     borderColor: 'rgba(255, 255, 255, 0.5)',
   },
   animation: {
@@ -91,7 +91,7 @@ export default StyleSheet.create({
   borderDesign: {
     width: 30,
     height: 30,
-    borderColor: 'white',
+    borderColor: '#CE00F6',
   },
   icon: {
     paddingRight: 10,
